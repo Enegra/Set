a91e1227bbfe98be760201c47f5cfc714a356b84
agnie <agnieszka.pula@gmail.com>
2016-12-21 18:58:07 +0100

    Yes, I did all inline declarations and it magically started working...

---
+++ Dog.h
@@ -0,0 +1,19 @@
+//
+// Created by agnie on 12/21/2016.
+//
+
+#ifndef SET_DOG_H
+#define SET_DOG_H
+
+#include <iostream>
+
+class Dog {
+private:
+    int size;
+    std::string color;
+public:
+    
+};
+
+
+#endif //SET_DOG_H
